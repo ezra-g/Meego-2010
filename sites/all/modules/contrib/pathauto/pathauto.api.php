7425fefa5f054c8552f2a2879067442e84637d96
Michael Shaver <michael.r.shaver@intel.com>
2010-08-20 11:11:25 -0700

    moving to pathauto (6.x-2.x-dev) as we need correct menu paths for page content types

--- sites/all/modules/contrib/pathauto/pathauto.api.php
+++ sites/all/modules/contrib/pathauto/pathauto.api.php
@@ -1,5 +1,5 @@
 <?php
-// $Id: pathauto.api.php,v 1.2.4.2 2010/08/09 19:01:33 davereid Exp $
+// $Id: pathauto.api.php,v 1.2.2.2 2010/08/09 18:57:29 davereid Exp $
 
 /**
  * @file
@@ -13,9 +13,6 @@ function hook_path_alias_types() {
 }
 
 function hook_pathauto($op) {
-}
-
-function hook_pathauto_bulkupdate() {
 }
 
 function hook_pathauto_alias_alter(&$alias, $context) {
