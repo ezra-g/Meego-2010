33114ce793504d547c07026f7adea4cf4b9d6fb2
Carl Wiedemann <c4rl@c4rl.local>
2010-09-28 13:34:43 -0600

    Some markup and CSS tweaks

--- sites/all/modules/contrib/cod_support/cod_session/cod-session-calendar.tpl.php
+++ sites/all/modules/contrib/cod_support/cod_session/cod-session-calendar.tpl.php
@@ -2,25 +2,25 @@
   <h2><?php print $day_title; ?></h2>
   <table class="session-calendar">
     <tr>
-      <th><?php print t('Time'); ?>\<?php print t('Room'); ?></th>
+      <th><?php print t('Time'); ?></th>
     <?php foreach ($rooms as $room_key => $room_title): ?>
       <th><?php print $room_title; ?></th>
     <?php endforeach ?>
     </tr>
     <?php foreach ($arranged_slots[$day_key] as $slot): ?>
       <tr class="<?php print $zebra = $zebra == 'even' ? 'odd':'even'; ?>">
-        <td>
+        <td class="time-label">
           <?php print $slot['start']; ?> -<br /><?php print $slot['end']; ?>
         </td>
         <?php foreach (array_keys($rooms) as $room_key): ?>
           <?php if (!empty($session_grid[$slot['nid']][$room_key]['session'])): ?>
-          <td class="occupied" <?php if (!empty($session_grid[$slot['nid']][$room_key]['colspan'])): ?>colspan="<?php print $session_grid[$slot['nid']][$room_key]['colspan']; ?>"<?php endif ?>>
+          <td class="session occupied" <?php if (!empty($session_grid[$slot['nid']][$room_key]['colspan'])): ?>colspan="<?php print $session_grid[$slot['nid']][$room_key]['colspan']; ?>"<?php endif ?>>
             <div class="views-item type-<?php print check_plain($session_grid[$slot['nid']][$room_key]['session']->type); ?>">
             <?php print $results[$session_grid[$slot['nid']][$room_key]['session']->nid]; ?>
             </div>
           </td>
           <?php elseif (empty($session_grid[$slot['nid']][$room_key]['spanned'])): ?>
-          <td class="empty">&nbsp;</td>
+          <td class="session empty">&nbsp;</td>
           <?php endif ?>
         <?php endforeach ?>
       </tr>
